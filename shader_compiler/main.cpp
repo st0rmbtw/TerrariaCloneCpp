b904377517c5f1b77c75f2a553f5cf878425d5d7
roman <carpowroma@gmail.com>
2024-08-05 00:40:17 +0300

    Some changes

--- shader_compiler/main.cpp
+++ shader_compiler/main.cpp
@@ -9,116 +9,7 @@
 
 namespace fs = std::filesystem;
 
-void ReadFromPipe(HANDLE pipe) { 
-   DWORD dwRead, dwWritten; 
-   CHAR chBuf[1024]; 
-   BOOL bSuccess = FALSE;
-   HANDLE hParentStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-
-   for (;;) 
-   { 
-      bSuccess = ReadFile(pipe, chBuf, 1024, &dwRead, NULL);
-      if (!bSuccess || dwRead == 0) break;
-
-      bSuccess = WriteFile(hParentStdOut, chBuf, dwRead, &dwWritten, NULL);
-      if (!bSuccess) break; 
-   } 
-}
-
-bool CompileVulkanShader(const std::string& executable, const std::string& stage, const char* shaderSource, size_t shaderSourceSize, const char* outputPath)
-{
-    SECURITY_ATTRIBUTES saAttr; 
-    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES); 
-    saAttr.bInheritHandle = TRUE; 
-    saAttr.lpSecurityDescriptor = NULL; 
-
-    HANDLE hStdInWr;
-    HANDLE hStdInRd;
-
-    HANDLE hStdOutWr;
-    HANDLE hStdOutRd;
-
-    bool success = CreatePipe(&hStdInRd, &hStdInWr, &saAttr, 0);
-    if (!success) {
-        printf("Couldn't create a pipe.\n");
-        return false;
-    }
-
-    success = SetHandleInformation(hStdInWr, HANDLE_FLAG_INHERIT, 0);
-    if (!success) {
-        printf("Couldn't set handle information.\n");
-        return false;
-    }
-
-    success = CreatePipe(&hStdOutRd, &hStdOutWr, &saAttr, 0);
-    if (!success) {
-        printf("Couldn't create a pipe.\n");
-        return false;
-    }
-
-    // success = SetHandleInformation(hStdOutRd, HANDLE_FLAG_INHERIT, 0);
-    // ASSERT(success, "ASDASDSd")
-
-    success = WriteFile(hStdInWr, shaderSource, shaderSourceSize, nullptr, NULL);
-    if (!success) {
-        printf("Couldn't write to the pipe.\n");
-        return false;
-    }
-
-    CloseHandle(hStdInWr);
-
-    STARTUPINFOA si;
-    PROCESS_INFORMATION pi;
-
-    ZeroMemory(&pi, sizeof(pi));
-
-    ZeroMemory(&si, sizeof(si));
-    si.cb = sizeof(si);
-    si.dwFlags |= STARTF_USESTDHANDLES;
-    si.hStdInput = hStdInRd;
-    si.hStdOutput = hStdOutWr;
-    si.hStdError = hStdOutWr;
-
-#if DEBUG
-    const std::string strip_debug_info = "";
-#else
-    const std::string strip_debug_info = " -g0";
-#endif
-
-    const std::string args = executable + strip_debug_info + " --quiet -V --enhanced-msgs --stdin -S " + stage + " -o " + outputPath;
-
-    // start the program up
-    success = CreateProcessA(
-        executable.c_str(), // the path
-        const_cast<char*>(args.c_str()),     // Command line
-        NULL,                   // Process handle not inheritable
-        NULL,                   // Thread handle not inheritable
-        TRUE,                  // Set handle inheritance to FALSE
-        CREATE_NO_WINDOW,     // Opens file in a separate console
-        NULL,           // Use parent's environment block
-        NULL,           // Use parent's starting directory 
-        &si,            // Pointer to STARTUPINFO structure
-        &pi           // Pointer to PROCESS_INFORMATION structure
-    );
-    if (!success) {
-        printf("Couldn't create a process.\n");
-        return false;
-    }
-
-    CloseHandle(hStdOutWr);
-
-    WaitForSingleObject(pi.hProcess, INFINITE);
-
-    ReadFromPipe(hStdOutRd);
-
-    // Close process and thread handles. 
-    CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
-    CloseHandle(hStdInRd);
-    CloseHandle(hStdOutWr);
-
-    return true;
-}
+bool CompileVulkanShader(const std::string& executable, const std::string& stage, const char* shaderSource, size_t shaderSourceSize, const char* outputPath);
 
 struct ShaderDef {
     std::string name;
@@ -201,3 +92,107 @@ int main(int argc, const char** argv) {
 
     return 0;
 }
+
+#ifdef WIN32
+
+void ReadFromPipe(HANDLE pipe) { 
+   DWORD dwRead, dwWritten; 
+   CHAR chBuf[1024]; 
+   BOOL bSuccess = FALSE;
+   HANDLE hParentStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+   for (;;) 
+   { 
+      bSuccess = ReadFile(pipe, chBuf, 1024, &dwRead, NULL);
+      if (!bSuccess || dwRead == 0) break;
+
+      bSuccess = WriteFile(hParentStdOut, chBuf, dwRead, &dwWritten, NULL);
+      if (!bSuccess) break; 
+   } 
+}
+
+bool CompileVulkanShader(const std::string& executable, const std::string& stage, const char* shaderSource, size_t shaderSourceSize, const char* outputPath)
+{
+    SECURITY_ATTRIBUTES saAttr; 
+    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES); 
+    saAttr.bInheritHandle = TRUE; 
+    saAttr.lpSecurityDescriptor = NULL; 
+
+    HANDLE hStdInWr;
+    HANDLE hStdInRd;
+
+    HANDLE hStdOutWr;
+    HANDLE hStdOutRd;
+
+    bool success = CreatePipe(&hStdInRd, &hStdInWr, &saAttr, 0);
+    if (!success) {
+        printf("Couldn't create a pipe.\n");
+        return false;
+    }
+
+    success = SetHandleInformation(hStdInWr, HANDLE_FLAG_INHERIT, 0);
+    if (!success) {
+        printf("Couldn't set handle information.\n");
+        return false;
+    }
+
+    success = CreatePipe(&hStdOutRd, &hStdOutWr, &saAttr, 0);
+    if (!success) {
+        printf("Couldn't create a pipe.\n");
+        return false;
+    }
+
+    // success = SetHandleInformation(hStdOutRd, HANDLE_FLAG_INHERIT, 0);
+    // ASSERT(success, "ASDASDSd")
+
+    success = WriteFile(hStdInWr, shaderSource, shaderSourceSize, nullptr, NULL);
+    if (!success) {
+        printf("Couldn't write to the pipe.\n");
+        return false;
+    }
+
+    CloseHandle(hStdInWr);
+
+    STARTUPINFOA si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&pi, sizeof(pi));
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    si.dwFlags |= STARTF_USESTDHANDLES;
+    si.hStdInput = hStdInRd;
+    si.hStdOutput = hStdOutWr;
+    si.hStdError = hStdOutWr;
+
+#if DEBUG
+    const std::string strip_debug_info = "";
+#else
+    const std::string strip_debug_info = " -g0";
+#endif
+
+    const std::string args = executable + strip_debug_info + " --quiet -V --enhanced-msgs --stdin -S " + stage + " -o " + outputPath;
+
+    // start the program up
+    success = CreateProcessA(executable.c_str(), const_cast<char*>(args.c_str()), NULL, NULL, TRUE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi);
+    if (!success) {
+        printf("Couldn't create a process.\n");
+        return false;
+    }
+
+    CloseHandle(hStdOutWr);
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    ReadFromPipe(hStdOutRd);
+
+    // Close process and thread handles. 
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    CloseHandle(hStdInRd);
+    CloseHandle(hStdOutWr);
+
+    return true;
+}
+
+#endif
